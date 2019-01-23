6ccb0d248155ee719c186d7481d8f277e0f2032b
Harald Nezbeda <hn@nezhar.com>
2019-01-23 22:39:55 +0100

    Add code highlight, add basic docs example

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { InstallComponent } from './pages/install/install.component';
 import { ContributeComponent } from './pages/contribute/contribute.component';
+import { ComponentComponent } from './pages/component/component.component';
 
 export const config = {
   title: 'ngx-documentary',
@@ -23,6 +24,13 @@ export const pages: Routes = [
     component: InstallComponent,
     data: {
       name: 'Install'
+    }
+  },
+  {
+    path: 'component',
+    component: ComponentComponent,
+    data: {
+      name: 'Component'
     }
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PrismModule } from '@ngx-prism/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './layout/app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { InstallComponent } from './pages/install/install.component';
 import { ContributeComponent } from './pages/contribute/contribute.component';
+import { ComponentComponent } from './pages/component/component.component';
+
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import 'prismjs/plugins/show-language/prism-show-language';
+
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-sass';
+import 'prismjs/components/prism-scss';
 
 @NgModule({
   declarations: [
@@ -14,10 +27,12 @@ import { ContributeComponent } from './pages/contribute/contribute.component';
     HomeComponent,
     InstallComponent,
     ContributeComponent,
+    ComponentComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule,
+    PrismModule,
     AppRoutingModule,
   ],
   providers: [],
